070c877c32d2c0d211eb34a42533ffa7d2be3235
Anders <andersoncarrera703@gmail.com>
2025-09-23 18:51:32 -0500

    Agregar archivos de proyecto.

---
+++ AAAA/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AAAA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,desa=0;
+            float nota;
+            Console.WriteLine("Ingrese el numero de notas");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Ingrese la nota del estudiante:" +(i+1) );
+                 nota = float.Parse(Console.ReadLine());
+                if (nota< 12) {
+                    desa++;
+                }
+                else if (nota)
+                {
+                    
+                }
+
+            }
+            Console.WriteLine("Hay " + desa + " estudiantes desaprobados");
+
+        }
+    }
+}
